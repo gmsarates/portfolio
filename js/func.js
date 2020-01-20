d736a2b1394036cb61ee661328623ad01d59c517
Gabriel Sarates <30908688+gmsarates@users.noreply.github.com>
2020-01-20 01:19:50 -0300

    Update func.js

--- js/func.js
+++ js/func.js
@@ -1,4 +1,4 @@
-$(document).on('click', 'a[href^="#"]', function (event) {
+$(document).on('click', 'a[href^="#content"]', function (event) {
   event.preventDefault();
 
   $('html, body').animate({
